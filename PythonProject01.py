56cbb639ed586feb91d6a85c58271885ff2c4ecb
SuYeongLee <rainbo3700@gmail.com>
2021-07-10 12:08:55 +0900

    Python기초(숫자형)"

---
+++ PythonProject01.py
@@ -0,0 +1,55 @@
+# 02-1 숫자형
+
+# 정수형
+a = 123
+print(a)
+a = -123
+print(a)
+a = 0
+print(a)
+
+# 실수형
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+
+# 컴퓨터식 지수 표현 방식
+a = 4.24E10
+print(a)
+a = 4.24e-10
+print(a)
+
+# 8진수와 16진수
+a = 0o177
+print(a)
+
+# 16진수
+a = 0x8ff
+print(a)
+b = 0xABC
+print(b)
+
+# 사칙연산
+a = 3
+b = 4
+print(a + b)
+print(a * b)
+print(a / b)
+
+# x의 y제곱을 나타내는  ** 연산자
+print(a ** b)
+
+# 나눗셈 후 나머지를 반환하는 % 연산자
+print(7 % 3)
+print(3 % 7)
+
+# 나눗셈 후 몫을 반환하는 // 연산자
+print(7 / 4)
+print(7 // 4)
+
+# 연습문제
+print(14 / 3)
+print(14 // 3)
+print(14 % 3)
+
