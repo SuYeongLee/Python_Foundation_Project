b8fbb32d61a19b1fd45216dd2cf4afcf37e50bcd
SuYeongLee <rainbo3700@gmail.com>
2021-07-12 19:15:54 +0900

    리스트 자료형

---
+++ PythonProject03.py
@@ -0,0 +1,38 @@
+# 리스트 자료형
+
+# 리스트는 어떻게 만들고 사용할까?
+odd = [1, 3, 5, 7, 9]
+print(odd)
+
+# 리스트명 = [요소1, 요소2, 요소3, ...]
+
+a = []
+b = [1, 2, 3]
+c = ['Life', 'is', 'too', 'short']
+d = [1, 2, 'Life', 'is']
+e = [1, 2, ['Life', 'is']]
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# 리스트의 인덱싱과 슬라이싱
+
+# 1. 리스트의 인덱싱
+a = [1, 2, 3]
+print(a)
+print(a[0])
+print(a[0] + a[2]) # 1 + 3
+print(a[-1])
+
+a = [1, 2, 3, ['a', 'b', 'c']]
+print(a[0])
+print(a[-1])
+print(a[3])
+
+print(a[-1][0])
+print(a[-1][1])
+print(a[-1][2])
+
