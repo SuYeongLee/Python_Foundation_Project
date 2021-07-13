a96d620862333bf28f258c7cb279e6002db070a4
SuYeongLee <rainbo3700@gmail.com>
2021-07-13 22:39:12 +0900

    리스트 자료형

--- PythonProject03.py
+++ PythonProject03.py
@@ -155,3 +155,16 @@ print(a)
 a = [1, 2, 3]
 print(a.pop(1))
 print(a)
+
+# 8. 리스트에 포함된 요소 x의 개수 세기(count)
+a = [1, 2, 3, 1]
+print(a.count(1))
+
+# 9. 리스트 확장(extend)
+a = [1, 2, 3]
+a.extend([4, 5])
+print(a)
+
+b = [6, 7]
+a.extend(b)
+print(a)
