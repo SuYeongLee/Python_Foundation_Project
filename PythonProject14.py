d0daf40acdbe91a59cd9ce196af144ee8d0d43e9
SuYeongLee <rainbo3700@gmail.com>
2021-07-18 00:53:15 +0900

    파일 읽고 쓰기

--- PythonProject14.py
+++ PythonProject14.py
@@ -90,7 +90,7 @@
 # for i in args:
 #     print(i)
 
-import sys
-args = sys.argv[1:]
-for i in args:
-    print(i.upper(), end=' ')
+# import sys
+# args = sys.argv[1:]
+# for i in args:
+#     print(i.upper(), end=' ')
