8803434cb8b35280bbb3e6b3cd8109d39f26e965
SuYeongLee <rainbo3700@gmail.com>
2021-07-19 22:04:21 +0900

    클래스

--- PythonProject15.py
+++ PythonProject15.py
@@ -75,83 +75,246 @@ class FourCal:
         self.first = first  # 2번 매서드의 수행문
         self.second = second
 
-    def add(self, numa, numb):
-        self.result = numa + numb
+    def add(self):
+        self.result = self.first + self.second
         return self.result
     
-    def mul(self, numa, numb):
-        self.result = numa * numb
+    def mul(self):
+        self.result = self.first * self.second
         return self.result
 
-    def sub(self, numa, numb):
-        self.result = numa - numb
+    def sub(self):
+        self.result = self.first - self.second
         return self.result
 
-    def div(self, numa, numb):
-        self.result = numa / numb
+    def div(self):
+        self.result = self.first / self.second
         return self.result
 
 a = FourCal()
+a.setdata(4, 2)
 
-print(a.add(4, 2))
-print(a.sub(4, 2))
-print(a.mul(4, 2))
-print(int(a.div(4, 2)))
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(int(a.div()))
 
-# 1. 객체에 숫자 지정할 수 있게 만들기
+
+# 클래스 구조 만들기
+
+# class FourCal:
+#     pass
+
+# a = FourCal()
+# print(type(a))
+
+# 객체에 숫자 지정할 수 있게 만들기
+
+# a.setdata(4, 2)
+
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
 
 # def 함수 이름(매개변수):
 #    수행할 문장
 
+# def setdata(self, first, second): # (1) 메서드의 매개변수
+    # (2) 메서드의 수행문
+#    self.first = first
+#    self.second = second
 
-# 메소드의 또 다른 호출 방법
-a = FourCal()
-FourCal.setdata(a, 4, 2)
+# (1)setdata 메서드의 매개변수
+# a = FourCal()
+# a.setdata(4, 2)
 
-a = FourCal()
-a.setdata(4, 2)
+# 메서드의 또 다른 호출 방법
 
-def setdata(self, first, second):
-    self.first = first
-    self.econd = second
+# a = FourCal()
+# FourCal.setdata(a, 4, 2)
 
+# a = FourCal()
+# a.setdata(4, 2)
 
-a = FourCal()
-a.setdata(4, 2)
-print(a.first)
-print(a.second)
+# (2)setdata 메서드의 수행문
+
+# def setdata(self, first, second): # (1) 메서드의 매개변수
+    # (2) 메서드의 수행문
+    # self.first = first
+    # self.second = second
+
+# self.first = 4
+# self.second = 2
+
+# a.first = 4
+# a.second = 2
+
+# a = FourCal()
+# a.setdata(4, 2)
+# print(a.first)
+# print(a.second)
+
+# a = FourCal()
+# b = FourCal()
+
+# a.setdata(4, 2)
+# print(a.first)
+
+# b.setdata(3, 7)
+# print(b.first)
+
+# print(a.first)
+
+# a = FourCal()
+# b = FourCal()
+# a.setdata(4, 2)
+# b.setdata(3, 7)
+# print(id(a.first)) # a의 first 주소 값을 확인
+# print(id(b.first)) # b의 first 주소 값을 확인
+
+# 더하기 기능 만들기
+
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+
+#     def add(self):
+#         result = self.first + self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4, 2)
+# print(a.add())
+
+# 곱하기, 빼기, 나누기 기능 만들기
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+#     def mul(self):
+#         result = self.first * self.second
+#         return result
+#     def sub(self):
+#         result = self.first - self.second
+#         return result
+#     def div(self):
+#         result = self.first / self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4, 2)
+# print(a.mul())
+# print(a.sub())
+# print(a.div())
 
 a = FourCal()
 b = FourCal()
 
 a.setdata(4, 2)
-print(a.first)
+b.setdata(3, 8)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(b.add())
+print(b.mul())
+print(b.sub())
+print(b.div())
 
-b.setdata(3, 7)
-print(b.first)
+# 생성자(Constructor)
 
-print(a.first)
-
-a = FourCal()
-b = FourCal()
-a.setdata(4, 2)
-b.setdata(3, 7)
-print(id(a.first)) # a의 first 주소 값을 확인
-print(id(b.first)) # b의 first 주소 값을 확인
-
-
-# 1. 더하기 기능 만들기
+# a = FourCal()
+# print(a.add())
 
 class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
     def setdata(self, first, second):
         self.first = first
         self.second = second
-
     def add(self):
         result = self.first + self.second
         return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
 
+a = FourCal(4, 2)
 
-a = FourCal()
-a.setdata(4, 2)
+# 매개 변수
+# self, first, second
+
+# 값
+# 생성되는 객체, 4, 2
+
+print(a.first)
+print(a.second)
+
+a = FourCal(4, 2)
 print(a.add())
+print(a.div())
+
+# 클래스의 상속
+# class 클래스 이름(상속할 클래스 이름)
+class MoreFourCal(FourCal):
+    pass
+
+a = MoreFourCal(4, 2)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+a = MoreFourCal(4, 2)
+print(a.pow())
+
+# 메서드 오버라이딩
+
+# a = FourCal(4, 0)
+# a.div()
+
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0: # 나누는 값인 0인 경우 숫자 0을 돌려주도록 수정
+            return 0
+        else:
+            return self.first / self.second
+
+a = SafeFourCal(4, 0)
+print(a.div())
+
+# 클래스 변수
+
+class Family:
+    lastname = "김"
+
+print(Family.lastname)
+
+a = Family()
+b = Family()
+print(a.lastname)
+print(b.lastname)
+
+Family.lastname = "박"
+print(a.lastname)
+print(b.lastname)
+
+# 모든 같은 메모리를 가리키고 있음
+
+print(id(Family.lastname))
+print(id(a.lastname))
+print(id(b.lastname))
